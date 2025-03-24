78093738281b84a601ba53829a60c78891bee696
gitcore33 <erickestanislau25@gmail.com>
2025-03-24 20:37:16 -0300

    adds partials and optmizes paths

--- helpers.php
+++ helpers.php
@@ -10,3 +10,35 @@
  function basePath($path = '') {
     return __DIR__ . '/' . $path;
  }
+
+ /**
+  * load a view
+  * @param string $name
+  *@return void
+  */
+
+  function loadView($name) {
+   $viewPath = basePath("views/{$name}.php");
+
+   if(file_exists($viewPath)) {
+      require $viewPath;
+   } else {
+      echo "View '{$name}' not found!";
+   }
+  }
+
+   /**
+  * load a partial
+  * @param string $name
+  *@return void
+  */
+
+  function loadPartial($name) {
+   $partialPath = basePath("views/partials/{$name}.php");
+
+   if(file_exists($partialPath)) {
+      require $partialPath;
+   } else {
+      echo "Partial '{$name}' not found!";
+   }
+  }
--- public/index.php
+++ public/index.php
@@ -2,4 +2,4 @@
 
 require '../helpers.php';
 
-require basePath('views/home.php');
+loadView('home');
