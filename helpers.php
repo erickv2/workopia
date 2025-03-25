a2151793e763c47c8ce51ca5a7e3fe2bace74c14
gitcore33 <erickestanislau25@gmail.com>
2025-03-25 17:01:14 -0300

    adds inspect helper functions

--- helpers.php
+++ helpers.php
@@ -42,3 +42,23 @@
       echo "Partial '{$name}' not found!";
    }
   }
+
+  /**
+   * inspect a value(s)
+   * 
+   * @param mixed $value
+   * @return void
+   */
+
+   function inspect($value) {
+      echo '<pre>';
+      var_dump($value);
+      echo '<pre>';
+   }
+
+   function inspectAndDie($value) {
+      echo '<pre>';
+      die(var_dump($value));
+      echo '<pre>';
+   }
+
