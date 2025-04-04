c20baf35b8db037d8dcacc9a042d32d86ee9f4ab
gitcore33 <erickestanislau25@gmail.com>
2025-04-03 21:28:02 -0300

    creates composer.json and install composer

--- public/index.php
+++ public/index.php
@@ -1,15 +1,16 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
 
 
-spl_autoload_register(function ($class) {
-    $path = basePath('Framework/' . $class . '.php');
+// spl_autoload_register(function ($class) {
+//     $path = basePath('Framework/' . $class . '.php');
 
-    if(file_exists($path)) {
-        require $path;
-    }
-});
+//     if(file_exists($path)) {
+//         require $path;
+//     }
+// });
 
 
 // instantiating the router
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'erickv2/workopia',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '582dba817339de033a5f291a597113bbca3ad998',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'erickv2/workopia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '582dba817339de033a5f291a597113bbca3ad998',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
