fae848ab352b16c94ddbd6f10e86343c7d242a4f
gitcore33 <erickestanislau25@gmail.com>
2025-04-25 21:17:38 -0300

    adds flash message on show page

--- App/views/listings/show.view.php
+++ App/views/listings/show.view.php
@@ -8,6 +8,7 @@ loadPartial('topbanner');
 
 <section class="container mx-auto p-4 mt-4">
       <div class="rounded-lg shadow-md bg-white p-3">
+      <?= loadPartial('message') ?>
        <div class="flex justify-between items-center">
       <a class="block p-4 text-blue-700" href="/listings">
         <i class="fa fa-arrow-alt-circle-left"></i>
