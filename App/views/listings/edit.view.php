1ca0611fe518fb8f03aa566da1f0d1201c2d9c93
gitcore33 <erickestanislau25@gmail.com>
2025-04-25 21:14:35 -0300

    updates listing values to database

--- App/views/listings/edit.view.php
+++ App/views/listings/edit.view.php
@@ -14,7 +14,8 @@ loadPartial('topbanner');
         <div class="message bg-green-100 p-3 my-3">
           This is a success message.
         </div> -->
-        <form method="POST" action="/listings">
+        <form method="POST" action="/listings/<?= $listing['id'] ?>">
+          <input type="hidden" name="_method" value="PUT">
           <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
             Job Info
           </h2>
