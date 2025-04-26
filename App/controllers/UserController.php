a5f62eb9b8f80e87c891916721a9f6b24e458a6b
gitcore33 <erickestanislau25@gmail.com>
2025-04-26 09:22:04 -0300

    create login and register routes and views

---
+++ App/controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php 
+
+namespace App\Controllers;
+
+use Framework\Database;
+use Framework\Validation;
+
+class UserController {
+    protected $db;
+
+    public function __construct() {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
+
+    /**
+     * show the login page
+     * 
+     * @return void
+     */
+
+     public function login() {
+        loadView('users/login');
+     }
+
+         /**
+     * show the register page
+     * 
+     * @return void
+     */
+
+     public function create() {
+        loadView('users/create');
+     }
+}
