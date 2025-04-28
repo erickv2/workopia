4d486d56cf587dc7f2894ef2adfeb36c84e1efe8
gitcore33 <erickestanislau25@gmail.com>
2025-04-28 20:50:46 -0300

    submits user to db

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -85,7 +85,7 @@ class UserController {
             'email' => $email
         ];
 
-        $user = $this->db->query('SELECT * FROM users WHERE email = :email', $params);
+        $user = $this->db->query('SELECT * FROM users WHERE email = :email', $params)->fetch();
 
         if($user) {
             $errors['email'] = 'Tha email already exists';
@@ -95,6 +95,21 @@ class UserController {
 
             exit;
         }
+
+        //create user account
+
+        $params = [
+            'name' => $name,
+            'email' => $email,
+            'city' => $city,
+            'state' => $state,
+            'password' => password_hash($password, PASSWORD_DEFAULT)
+        ];
+
+        $this->db->query('INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)', $params);
+
+        redirect('/');
+
       }
 
 }
