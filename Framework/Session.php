22b2dd41d16128333e74f1249219e6d2b1532a36
gitcore33 <erickestanislau25@gmail.com>
2025-04-29 10:35:03 -0300

    creates session class an methods and implements on index

---
+++ Framework/Session.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Framework;
+
+class Session {
+    /**
+     * start the session
+     * 
+     * @return void
+     */
+
+     public static function start() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+     }
+
+     /**
+      * set a session key/value pair
+      * @param string $key
+      * @param mixed $value
+      * @return void
+      */
+
+     public static function set($key, $value) {
+        $_SESSION[$key] = $value;
+     }
+
+    /**
+      * get a session value by the key
+      * @param string $key
+      * @param mixed $default
+      * @return void
+      */
+
+     public static function get($key, $default = null) {
+        return isset($_SESSION[$key]) ? $_SESSION[$key] : $default;
+     }
+
+    /**
+      * check if session key exists
+      * @param string $key
+      * @return bool
+      */
+
+      public static function has($key) {
+        return isset($_SESSION[$key]);
+      }
+
+    /**
+      * clear session by key
+      * @param string $key
+      * @return void
+      */
+
+      public static function clear($key) {
+        if(isset($_SESSGION[$key])) {
+            unset($_SESSION[$key]);
+        }
+      }
+
+    /**
+      * clear all session data
+      * @return void
+      */
+
+      public function clearAll() {
+        session_unset();
+        session_destroy();
+      }
+}
