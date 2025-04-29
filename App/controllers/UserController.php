18835d03db1e405b325f42645a233d7cc94f2094
gitcore33 <erickestanislau25@gmail.com>
2025-04-29 10:41:59 -0300

    implements session

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Framework\Database;
 use Framework\Validation;
+use Framework\Session;
 
 class UserController {
     protected $db;
@@ -107,6 +108,18 @@ class UserController {
         ];
 
         $this->db->query('INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)', $params);
+
+        // get new user ID
+
+        $userId = $this->db->conn->lastInsertId();
+
+        Session::set('user', [
+            'id' => $userId,
+            'name' => $name,
+            'email' => $email,
+            'city' => $city,
+            'state' => $state
+        ]);
 
         redirect('/');
 
